41c5995e074d572fbdcfb6bfe912b32fdb055e3d
Dino Jovanov <jovanovdino@gmail.com>
2020-01-30 18:55:32 +0100

    14

---
+++ Vidly/Models/RoleName.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Bookie.Models
+{
+    public static class RoleName
+    {
+        public const string CanManageMovies = "CanManageMovies";
+    }
+}
---
+++ Vidly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bookie.Startup))]
+namespace Bookie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
